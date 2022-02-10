54a53f7fd57455c44ed3001a153e97d9a55f36c5
Milats <israestan@gmail.com>
2022-02-09 20:16:46 -0500

    20:15

--- app/src/main/java/com/fisei/athanasiaapp/LoginActivity.java
+++ app/src/main/java/com/fisei/athanasiaapp/LoginActivity.java
@@ -2,6 +2,7 @@ package com.fisei.athanasiaapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
+import android.content.res.Resources;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
@@ -21,6 +22,8 @@ import org.json.JSONObject;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -37,14 +40,18 @@ public class LoginActivity extends AppCompatActivity {
         InitializeViewComponents();
     }
     private void Login(){
+        if(SIMP_CheckIfPasswordIsValid(passwdEditText.getText().toString())) {
             warningTextView.setText("");
             LoginTask loginTask = new LoginTask();
             loginTask.execute();
+        }
     }
     private Boolean LoginAdmin(){
+        if(SIMP_CheckIfPasswordIsValid(passwdEditText.getText().toString())){
             warningTextView.setText("");
             LoginAdminTask loginAdminTask = new LoginAdminTask();
             loginAdminTask.execute();
+        }
         return true;
     }
     private void SignUp(){
@@ -127,4 +134,83 @@ public class LoginActivity extends AppCompatActivity {
     private final View.OnClickListener signUpButtonClicked = view -> SignUp();
     private final View.OnLongClickListener loginAdminButtonClicked = view -> LoginAdmin();
 
+    private boolean SIMP_CheckIfPasswordIsValid(String passwd){
+        String error_message = "";
+        boolean flag = true;
+        Resources res = getResources();
+        if(!SIMP_CheckPasswLenght(passwd)){
+            flag = false;
+            error_message += "\n" + res.getString(R.string.password_no_lenght);
+        }
+        if (!SIMP_CheckPasswLowerCase(passwd)){
+            flag = false;
+            error_message += "\n" + res.getString(R.string.password_no_lower);
+        }
+        if(!SIMP_CheckPasswUpperCase(passwd)){
+            flag = false;
+            error_message += "\n" + res.getString(R.string.password_no_upper);
+        }
+        if (!SIMP_CheckPasswSpecial(passwd)){
+            flag = false;
+            error_message += "\n" + res.getString(R.string.password_no_special);
+        }
+        if(!SIMP_CheckPasswNumber(passwd)){
+            flag = false;
+            error_message += "\n" + res.getString(R.string.password_no_digit);
+        }
+        warningTextView.setText(error_message);
+        return flag;
+    }
+    private boolean SIMP_CheckPasswLenght(String passwd){
+        return passwd.length() >= 6 && passwd.length() <= 10;
+    }
+    private boolean SIMP_CheckPasswUpperCase(String passwd){
+        char c;
+        for (int i = 0; i < passwd.length(); i++) {
+            c = passwd.charAt(i);
+            if (Character.isUpperCase(c)) {
+                return true;
+            }
+        }
+        return false;
+    }
+    private boolean SIMP_CheckPasswLowerCase(String passwd) {
+        char c;
+        for (int i = 0; i < passwd.length(); i++) {
+            c = passwd.charAt(i);
+            if (Character.isLowerCase(c)) {
+                return true;
+            }
+        }
+        return false;
+    }
+    private boolean SIMP_CheckPasswSpecial(String passwd){
+        Pattern p = Pattern.compile("[^a-z0-9 ]", Pattern.CASE_INSENSITIVE);
+        Matcher m = p.matcher(passwd);
+
+        return m.find();
+    }
+    private boolean SIMP_CheckPasswNumber(String passwd) {
+        char c;
+        for (int i = 0; i < passwd.length(); i++) {
+            c = passwd.charAt(i);
+            if (Character.isDigit(c)) {
+                return true;
+            }
+        }
+        return false;
+    }
+    private boolean SIMP_CheckCedulaLenght(String cedula){
+        return cedula.length() == 10;
+    }
+    private boolean SIMP_CheckCedulaOnlyDigits(String cedula){
+        char c;
+        for (int i = 0; i < cedula.length(); i++) {
+            c = cedula.charAt(i);
+            if (!Character.isDigit(c)) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
